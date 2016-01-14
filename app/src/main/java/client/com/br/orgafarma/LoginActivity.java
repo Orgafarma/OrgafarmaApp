30a0c6b93befd872c3bb8b5d89fc7b0f6d65881f
Orgafarma <rodolfo.rezende@orgafarma.com.br>
2016-01-14 17:12:13 -0200

    COMMIT RODOLFO

--- app/src/main/java/client/com/br/orgafarma/LoginActivity.java
+++ app/src/main/java/client/com/br/orgafarma/LoginActivity.java
@@ -19,7 +19,7 @@ import Util.MensagemUtil;
  * Created by Felipe on 16/11/2015.
  */
 public class LoginActivity extends AppCompatActivity {
-    //
+
     private LoginBO loginBO;
     private EditText edtlogin;
     private EditText edtSenha;
