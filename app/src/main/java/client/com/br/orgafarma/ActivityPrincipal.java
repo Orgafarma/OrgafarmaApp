239ec7c513b5e9ddadb762884a9c993373091057
Felipe Lemos <felipe.lemos.almeida@gmail.com>
2016-01-25 14:40:27 -0200

    Resolvendo problema checkde MenuItem

--- app/src/main/java/client/com/br/orgafarma/ActivityPrincipal.java
+++ app/src/main/java/client/com/br/orgafarma/ActivityPrincipal.java
@@ -93,13 +93,13 @@ public class ActivityPrincipal extends AppCompatActivity {
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
                         switch (menuItem.getItemId()) {
                             case R.id.navigation_sub_item_1:
-                                menuItem.setChecked(true);
+                                /*menuItem.setChecked(true);*/
                                 setFragment(R.id.navigation_sub_item_1);
                                 drawerLayout.closeDrawer(GravityCompat.START);
                                 return true;
 
                             case R.id.navigation_sub_item_2:
-                                menuItem.setChecked(true);
+                                /*menuItem.setChecked(true);*/
                                 setFragment(R.id.navigation_sub_item_2);
                                 drawerLayout.closeDrawer(GravityCompat.START);
                                 return true;
